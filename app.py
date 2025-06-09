0c4ecdbfb273be89f6908d9df1fadf86056a3d7d
unknown <alecsander.castro@clicnicaseven.com.br>
2025-06-09 11:01:41 -0300

    feat(*): add create user function

--- app.py
+++ app.py
@@ -1,6 +1,37 @@
 from flask import Flask, jsonify, request
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
+GROUP_ID = os.getenv("GROUP_ID")
+
+#Criar usuário
+def create_user():
+    data = request.json
+
+    payload = {
+        "groupId": GROUP_ID,
+        "userPhone": data['userPhone'],
+        "email": data['email'],
+        "expireAt": data.get('expireAt'),
+        "removeAt": data.get('removeAt')   
+    }
+
+    headers = {
+      "Authorization": "Bearer {BEARER_TOKEN}",
+      "Content-type":"application/json"
+    }
+
+    response = request.post(
+        "https://api.dieta.ai/v1/groups/users/add",
+        json=payload,
+        headers=headers
+    )
+
+    return jsonify(response.json()), response.status_code
 
 app.run(port=5000, host='localhost', debug=True)
